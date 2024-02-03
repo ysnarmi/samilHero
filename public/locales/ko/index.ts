7fd542ae741aee9a2e2de8cf27ff777cdfa21101
juchanhwang <ghkdwncks96@gmail.com>
2024-02-03 18:11:27 +0900

    initial commit

--- public/locales/ko/index.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-
-import common from './common.json'
-
-export default {
-    common,
-}
--- src/features/common/constants/routes.ts
+++
@@ -1,8 +0,0 @@
-// 대시보드
-export const ROOT_PATH = '/'
-
-// error
-export const E404_PATH = '/404'
-
-// 로그인
-export const LOGIN_PATH = '/login'
